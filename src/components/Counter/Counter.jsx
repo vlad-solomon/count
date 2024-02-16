5db92d03da1212884cda2f5351f642a9c3f20efc
Vlad Solomon <vladsolomon720@gmail.com>
2024-02-16 23:41:10 +0200

    Create active state of Counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,17 +1,21 @@
 import "./Counter.scss";
+import { useState } from "react";
+import classNames from "classnames";
 
-export function Counter() {
+export function Counter({ isActive }) {
+	const [count, setCount] = useState(0);
+
 	return (
-		<div className="counter">
+		<div className={classNames("counter", isActive && "counter--active")}>
 			<div className="counter__icon">👋</div>
 			<div className="counter__details">
 				<div className="counter__title">Item name</div>
 				<div className="counter__unit">unit of measurement</div>
 			</div>
 			<div className="counter__counter-display">
-				<button>-</button>
-				<div className="counter__value">13</div>
-				<button>+</button>
+				<button onClick={() => setCount((prev) => --prev)}>-</button>
+				<div className="counter__value">{count}</div>
+				<button onClick={() => setCount((prev) => ++prev)}>+</button>
 			</div>
 		</div>
 	);
